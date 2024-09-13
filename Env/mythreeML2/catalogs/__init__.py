7f91971747733f3b13d93f156476499e31c91d66
xiahouwenyu <736492048@qq.com>
2024-09-13 19:43:31 +0800

    too much

--- Env/mythreeML2/catalogs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .FermiGBM import FermiGBMBurstCatalog, FermiGBMTriggerCatalog
-from .FermiLLE import FermiLLEBurstCatalog
-from .FermiLAT import FermiLATSourceCatalog, FermiPySourceCatalog
-from .Swift import SwiftGRBCatalog
